b239b8aa595cbeac5225b224cda8db8ad0e8af33
Ballsky22 <ballskyteam22@gmail.com>
2023-04-01 14:24:33 +0700

    Bright - String

--- Data Type and Operator/script.js
+++ Data Type and Operator/script.js
@@ -1,9 +1,20 @@
 // Array
-let a = ["Ari", "Ballsky", "Miki"];
-console.log(a);
+// let a = ["Ari", "Ballsky", "Miki"];
+// console.log(a);
 
 // TypeOf Mencari tau dari variable yang di tuliskan
-const a = "Ballsky";
-const b = 23;
-const c = true;
-console.log(typeof c);
+// const a = "Ballsky";
+// const b = 23;
+// const c = true;
+// console.log(typeof c);
+
+// String
+// Backtrics harus memakai tanda dollar $
+// let a = "${1 + 2}";
+// let a = `${1 + 2}`;
+
+let str = "Ari";
+let str2 = "Budi";
+let name = `The Names are ${str} and ${str2}`;
+console.log(name);
+console.log(`result: ${1 + 2}`);
