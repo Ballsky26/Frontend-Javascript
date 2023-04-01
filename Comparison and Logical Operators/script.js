988c14d8a48fbb3b29622efc72f183c09a5c3cb8
Ballsky22 <ballskyteam22@gmail.com>
2023-04-01 15:11:06 +0700

    Logical Operator OR ||

--- Comparison and Logical Operators/script.js
+++ Comparison and Logical Operators/script.js
@@ -8,9 +8,24 @@
 // console.log(result);
 
 // Operator Srict Equal
-const a = 2;
+// const a = 2;
+// console.log(a === 2);
+// console.log(a === "2");
+// console.log(0 === false);
+// console.log("" === false);
 
-console.log(a === 2);
-console.log(a === "2");
-console.log(0 === false);
-console.log("" === false);
+// Operator Or (||) Jika salah satunya true maka hasilnya akan true
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+let a = true;
+let b = false;
+console.log(a || b);
+
+let c = 9;
+if (c < 10 || c > 20) {
+  console.log(c);
+}
