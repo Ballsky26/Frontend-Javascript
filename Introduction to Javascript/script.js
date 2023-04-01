a2bcdef5cf850dbe4cbb11e4b62c90bbc7192072
Ballsky22 <ballskyteam22@gmail.com>
2023-04-01 13:53:59 +0700

    Introduction to Javascript

---
+++ Introduction to Javascript/script.js
@@ -0,0 +1,26 @@
+// var a = 1;
+// var a = 2;
+
+// let a = 1;
+// a = 2;
+
+// const a = 1;
+// a = 2;
+
+// {
+//   const a = 1;
+//   let b = 2;
+//   var c = 3;
+//   console.log(a, b, c);
+// }
+// yang cuman bisa kebaca adalah yang variable var
+// {
+//   console.log(c);
+// }
+
+const obj = { id: 1, name: "Ballsky" };
+// obj.age = 23;
+const array = [1, 2, 3];
+array.push(5);
+
+console.log(array);
