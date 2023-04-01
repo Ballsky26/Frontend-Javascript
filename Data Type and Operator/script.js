0a63539e96e6f0adff226470be9a849e9b0d4136
Ballsky22 <ballskyteam22@gmail.com>
2023-04-01 14:30:59 +0700

    Boolean - Null

--- Data Type and Operator/script.js
+++ Data Type and Operator/script.js
@@ -13,8 +13,18 @@
 // let a = "${1 + 2}";
 // let a = `${1 + 2}`;
 
-let str = "Ari";
-let str2 = "Budi";
-let name = `The Names are ${str} and ${str2}`;
-console.log(name);
-console.log(`result: ${1 + 2}`);
+// let str = "Ari";
+// let str2 = "Budi";
+// let name = `The Names are ${str} and ${str2}`;
+// console.log(name);
+// console.log(`result: ${1 + 2}`);
+
+// Boolean
+// let a = 1;
+// let b = 2;
+// console.log(a > b);
+
+// Null
+let a = null;
+let b = 2;
+console.log(a > b);
