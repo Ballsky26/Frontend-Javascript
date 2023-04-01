2cd029f78aab0dd9e341d6cf60456b53c4af2f01
Ballsky22 <ballskyteam22@gmail.com>
2023-04-01 15:00:41 +0700

    Comparison Object

---
+++ Comparison and Logical Operators/script.js
@@ -0,0 +1,16 @@
+// Comparison Operator
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+
+// let result = 5 > 4;
+// console.log(result);
+
+// Operator Srict Equal
+const a = 2;
+
+console.log(a === 2);
+console.log(a === "2");
+console.log(0 === false);
+console.log("" === false);
