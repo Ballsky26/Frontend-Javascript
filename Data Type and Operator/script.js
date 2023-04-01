724f5bd0d91e91edd8161d686569539240611c05
Ballsky22 <ballskyteam22@gmail.com>
2023-04-01 14:14:45 +0700

    Tipe Data - Number

---
+++ Data Type and Operator/script.js
@@ -0,0 +1,9 @@
+// Array
+let a = ["Ari", "Ballsky", "Miki"];
+console.log(a);
+
+// TypeOf Mencari tau dari variable yang di tuliskan
+const a = "Ballsky";
+const b = 23;
+const c = true;
+console.log(typeof c);
