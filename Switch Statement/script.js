cac9058e517d33679877fb9f730793976e3eeb62
Ballsky22 <ballskyteam22@gmail.com>
2023-04-02 14:46:07 +0700

    Switch Statement

---
+++ Switch Statement/script.js
@@ -0,0 +1,44 @@
+// Break
+// let a = 3;
+// switch (a) {
+//   case 3:
+//     console.log("Kecil");
+//     break;
+//   case 4:
+//     console.log("Menengah");
+//     break;
+//   case 5:
+//     console.log("Besar");
+//     break;
+//   default:
+//     console.log("angka tidak ada");
+// }
+
+// Without Break
+// let a = 3;
+// switch (a) {
+//   case 3:
+//     console.log("Kecil");
+//   case 4:
+//     console.log("Menengah");
+//   case 5:
+//     console.log("Besar");
+//   default:
+//     console.log("angka tidak ada");
+// }
+
+// Strict Comparison
+let a = "1";
+switch (a) {
+  case "1":
+    console.log("Ini adalah string");
+    break;
+  case 1:
+    console.log("Ini adalah number");
+    break;
+  case 2:
+    console.log("Ini adalah number");
+    break;
+  default:
+    console.log("ini default");
+}
