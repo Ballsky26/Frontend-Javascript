e17b4e62acc87050c3a762a90727aea964e4eee1
Ballsky22 <ballskyteam22@gmail.com>
2023-04-02 14:14:31 +0700

    Conditional Branching and Switch Statement

---
+++ Conditional Branching and Switch Statement/script.js
@@ -0,0 +1,44 @@
+// let nextyear = 2023;
+// let previousyear = 2021;
+// let a = nextyear > 2022;
+// let b = previousyear < 2022;
+
+// if (a) {
+//   console.log("Betul ini lebih dari 2022");
+// } else if (b) {
+//   console.log("bukan 2022");
+// } else {
+//   console.log("Gak ada yang match");
+// }
+// console.log("Bisa Langsung kesini");
+
+// If
+let number = 2;
+if (number > 0) {
+  console.log("Ya benar");
+}
+
+// If else
+let tahun = 2022;
+if (tahun == 2023) {
+  console.log("Ya ini tahun 2023");
+} else {
+  console.log("ini bukan tahun 2023");
+}
+
+// If Else If
+let umur = 15;
+if (umur > 18) {
+  console.log("Area 18++");
+} else if (umur < 18) {
+  console.log("Belum Cukup Umur");
+} else {
+  console.log("Anda masih bocil");
+}
+
+// Konversi Boolean
+let year = 2024;
+let a = year == 2022;
+if (a) {
+  console.log("test");
+}
