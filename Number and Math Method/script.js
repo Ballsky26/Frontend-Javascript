3070012dfd89aeb74e5fd7fe3b16bb52cc710278
Ballsky22 <ballskyteam22@gmail.com>
2023-04-05 10:54:30 +0700

    Number and Math Method

---
+++ Number and Math Method/script.js
@@ -0,0 +1,68 @@
+// Number
+// let a = 99999999999999999;
+// let a = 0.2 + 01;
+// let a = (0.2 * 10 + 0.1 * 10) / 10;
+// let a = 10;
+// let b = 20;
+// console.log(a + b);
+
+// toString
+// let a = 10;
+// let b = "20";
+// console.log(a);
+// console.log(b.toString());
+
+// toExponential : mengembalikan string dengan angka yang dibulatkan
+// let x = 9.656;
+// console.log(x.toExponential());
+// console.log(x.toExponential(2));
+// console.log(x.toExponential(4));
+
+// toFixed : pebulatan mengembalikan string dengan nomor yang di tulis dengan jumlah desimal yang ditentukan
+// let x = 9.656;
+// console.log(x.toFixed());
+// console.log(x.toFixed(2));
+// console.log(x.toFixed(4));
+
+// toPrecision : mengembalikan string, dengan nomor yg ditulis dengan panjang tertentu
+// let x = 9.656;
+// console.log(x.toPrecision());
+// console.log(x.toPrecision(2));
+// console.log(x.toPrecision(4));
+
+// Math method
+// Math round : mengembalikan bilangan bulat terdekat
+// let a = 9.566461;
+// console.log(Math.round(a));
+
+// Math ceil : mengembalikan bilangan bulat ke atas terdekat
+// let a = 9.566461;
+// console.log(Math.ceil(a));
+
+// Math floor : mengembalikan bilangan bulat ke bawah terdekat
+// let a = 9.566461;
+// console.log(Math.floor(a));
+
+// Math trunc : mengembalikan bilangan integer yang depan (depannya saja)
+// let a = 9.566461;
+// console.log(Math.trunc(a));
+
+// Math pow : mengembalikan nilai ke pangkat y
+// let a = 7;
+// let b = 2;
+// console.log(Math.pow(a, b));
+
+// Math sqrt : mengembalikan nilai ke pangkat x
+// let a = 7;
+// let b = 2;
+// console.log(Math.sqrt(a, b));
+
+// Math min : menemukan nilai terendah
+// console.log(Math.min(0, 150, -300, -200, 100));
+
+// Math min : menemukan nilai terendah
+// console.log(Math.max(0, 150, -300, -200, 100));
+
+// Math random : menemukan nilai acak
+// console.log(Math.random());
+console.log(Math.floor(Math.random() * 10));
